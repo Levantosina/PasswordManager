e3c21d298ec43c8ffcb617861580393662fdd935
levan <levantosina1992@gmail.com>
2025-04-25 18:31:55 +0300

    add new requestMatchers

--- backend/src/main/java/com/password/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/password/security/SecurityFilterChainConfig.java
@@ -4,6 +4,7 @@ package com.password.security;
 import com.password.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,7 +29,10 @@ public class SecurityFilterChainConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/users/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/v1/users/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT,"/api/v1/users/**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE,"/api/v1/users/**").authenticated()
+                        .requestMatchers(HttpMethod.GET,"/api/v1/passwords/**").authenticated()
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
