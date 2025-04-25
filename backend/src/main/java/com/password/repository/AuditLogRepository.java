51da4ca8d2dbb26c3d1a7dd134d5843061a5685b
levan <levantosina1992@gmail.com>
2025-04-25 18:24:28 +0300

    add  findAllByUserAndDeletedFalse to AuditLogRepository

--- backend/src/main/java/com/password/repository/AuditLogRepository.java
+++ backend/src/main/java/com/password/repository/AuditLogRepository.java
@@ -1,7 +1,11 @@
 package com.password.repository;
 
 import com.password.model.AuditLogEntity;
+import com.password.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuditLogRepository extends JpaRepository<AuditLogEntity,Long> {
+    List<AuditLogEntity> findAllByUserAndDeletedFalse(UserEntity user);
 }
